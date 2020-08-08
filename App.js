6b38e85da2c70ed0ec8212c85a7cd60a2f5fc151
Aaditya Dhruv <aadityadhruv11@gmail.com>
2020-08-08 14:10:15 +0530

    Login screen, registration screen completed, basic functions to connect to server also added

--- App.js
+++ App.js
@@ -2,30 +2,47 @@ import { NavigationContainer, Link } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen/HomeScreen.js';
+import 'react-native-gesture-handler';
+import { useEffect, useState } from 'react'
+import { LoginScreen, HomeScreen, RegistrationScreen} from './src/screens'
+import { decode, encode } from 'base-64'
+
 import Groups from './src/screens/Groups/Groups.js';
 import Profile from './src/screens/Profile/Profile';
 import Chats from './src/screens/Chats/Chats';
 import CreateGroup from './src/screens/Groups/CreateGroup.js';
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+
+if (!global.btoa) { global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+
 
 const Stack = createStackNavigator();
-function App (){
-  return (
-  <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Groups" component={Groups} />
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Chats" component={Chats} />
-        <Stack.Screen name="CreateGroup" component={CreateGroup} />
+function App() {
 
-        </Stack.Navigator>
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {user ? (
+          <Stack.Screen name="Home">
+            {props => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Registration" component={RegistrationScreen} />
+              <Stack.Screen name="Groups" component={Groups} />
+              <Stack.Screen name="Profile" component={Profile} />
+              <Stack.Screen name="Chats" component={Chats} />
+              <Stack.Screen name="CreateGroup" component={CreateGroup} />
+
+            </>
+          )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -38,3 +55,8 @@ const styles = StyleSheet.create({
 });
 
 export default App
+
+
+/*
+
+*/
