47620db420460d3c7516449c816193fb45a8fe09
Aaditya Dhruv <aadityadhruv11@gmail.com>
2020-08-10 18:35:29 +0530

    completed joining and leaving group (needs testing)

--- src/screens/Groups/JoinGroup.js
+++ src/screens/Groups/JoinGroup.js
@@ -2,10 +2,77 @@ import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Button, Settings,TextInput } from 'react-native';
 //import firebase from 'firebase';
 
+import firebase from 'firebase'
 function JoinGroup({ navigation, route }) {
+
+
+
+    let unsubscribe;
+
+
+    class FirebaseInfo extends React.Component {
+        state = { groupsList: [], loading: true, isJoined : false ,search:"" };
+
+        componentDidMount() {
+            var user = firebase.auth().currentUser;
+            var db = firebase.firestore();
+
+            var userInfoRef = db.collection("Users").doc(user.uid);
+            unsubscribe = userInfoRef.onSnapshot((doc) => {
+                
+
+                this.setState({ groupsList: doc.data().groupsList, loading : false, displayedList: doc.data().groupsList});
+      //          console.log(this.state.groupIDs);
+
+            });
+
+            if (this.state.groupsList.includes(route.params.id))  {
+                this.setState({isJoined : true});
+            }
+            console.log(this.state.isJoined);
+            setRequest(this.state.isJoined);
+         // 
+        }
+        componentWillMount() {
+            return unsubscribe;
+          }
+        render() {
+        return (<View></View>);
+        }
+
+    }   
+
+
+
+
+function addGroup() {
+    var user = firebase.auth().currentUser;
+            var db = firebase.firestore();
+
+            var userInfoRef = db.collection("Users").doc(user.uid);
+            userInfoRef.update({
+                "groupsList" : firebase.firestore.FieldValue.arrayUnion({id : route.params.id, name : route.params.name})
+            })
+}
+function removeGroup() {
+    var user = firebase.auth().currentUser;
+            var db = firebase.firestore();
+
+            var userInfoRef = db.collection("Users").doc(user.uid);
+            userInfoRef.update({
+                "groupsList" : firebase.firestore.FieldValue.arrayRemove({id : route.params.id, name : route.params.name})
+            })
+}
+
     function login_new() {
         //groupID.status = "Requested"
-        setText(" ")
+        
+        !request ? setText("Leave") : setText("Join " + groupName);
+        !request ? addGroup() : removeGroup();
+        setRequest(!request);
+        
+
+        
    //     navigation.navigate('HomeScreen')
     }
     var groupID = route.params.id;
@@ -13,20 +80,22 @@ function JoinGroup({ navigation, route }) {
     console.log("ID: " + groupID);
     // get name , classes , optional description , status of the group from database
     var txt2=''
-    const [text, setText] = React.useState(txt2)
+    const [text, setText] = React.useState("Join " + groupName)
     const [text2, setText2] = React.useState(txt2)
     const [text3, setText3] = React.useState(txt2)
-    console.log(groupID)
+    const [request, setRequest] = React.useState(false)
+    console.log(groupID);
     return (
         <View>
             <Text style = {styles.AnswerText}>{groupName}</Text>
             <Text style = {styles.AnswerText}>Class = {route.params.label}</Text>
             <Text style = {styles.AnswerText}>Description = {route.params.desc}</Text>
+            <FirebaseInfo>  </FirebaseInfo>
 
             <TouchableOpacity style={styles.AnswerButtonBlack} onPress={() => { login_new() }}>
-        <Text style={styles.LoginText}>{"Join " + groupName}</Text>
+        <Text style={styles.LoginText}>{text}</Text>
       </TouchableOpacity>     
-      <Text style={styles.LoginText}>{text}</Text>
+      
        
         </View>
     )
